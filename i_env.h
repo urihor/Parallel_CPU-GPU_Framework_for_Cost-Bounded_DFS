7d4136dce95024ecb51c457d712ec62be7802fd3
Uriel <urielhor@gmail.com>
2025-11-05 14:35:26 +0200

    add interface of environment

---
+++ i_env.h
@@ -0,0 +1,16 @@
+//
+// Created by uriel on 04/11/2025.
+//
+
+#pragma once
+#include <vector>
+
+template<class S, class A>
+struct i_env {
+    // כל הסרצ'ר עובד מול הממשק הזה בלבד:
+    virtual std::vector<A> GetActions(const S& s) = 0; // מהלכים חוקיים ממצב s
+    virtual void ApplyAction(S& s, A a) = 0;           // מבצע מהלך אחד (in-place)
+    virtual void UndoAction(S& s, A a) = 0;            // מבטל מהלך
+    virtual bool IsGoal(const S& s) const = 0;         // האם s הוא מטרה
+    virtual ~i_env() = default;
+};
